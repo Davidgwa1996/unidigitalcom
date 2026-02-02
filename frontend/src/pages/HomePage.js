5cd0f61087ac7e250af9644998d765b01b332011
David Njau Maina <davidmaina3713413@gmail.com>
2026-02-02 18:44:39 +0000

    FIX: Removed Header/Footer from HomePage to prevent duplication

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import Header from '../components/Header';
 import Hero from '../components/Hero';
 import ProductGrid from '../components/ProductGrid';
-import Footer from '../components/Footer';
 
 function HomePage() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow">
-        <Hero />
-        <ProductGrid />
-        {/* Additional sections can be added here */}
-      </main>
-      <Footer />
+    <div className="flex-grow">
+      <Hero />
+      <ProductGrid />
+      {/* Additional sections can be added here */}
     </div>
   );
 }
